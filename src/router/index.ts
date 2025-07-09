e9113aa600467a7f4b2e7bddff8cbf04166f90a3
Sourabh Mandal <19mandal97@gmail.com>
2025-07-09 22:02:35 +0530

    fix: add navbar

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import DashboardView from '@/views/DashboardView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/d',
+      name: 'dashboard',
+      component: DashboardView,
     },
     {
       path: '/about',
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-black">
+    <h1>This is an Dashboard page</h1>
+    <h1>This is an Dashboard page</h1>
+    <h1>This is an Dashboard page</h1>
+    <h1>This is an Dashboard page</h1>
+    <h1>This is an Dashboard page</h1>
+    <h1>This is an Dashboard page</h1>
+    <h1>This is an Dashboard page</h1>
+    <h1>This is an Dashboard page</h1>
+    <h1>This is an Dashboard page</h1>
+    <h1>This is an Dashboard page</h1>
+    <h1>This is an Dashboard page</h1>
+  </div>
+</template>
